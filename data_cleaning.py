8d29240e490dd47c6ae0abd9bd3970ac8f4913db
sill-vaino <73522648+sill-vaino@users.noreply.github.com>
2023-12-11 06:07:29 +0200

    Update data_cleaning.py
    Had to correct the columns and where the info comes

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,28 +1,24 @@
 import pandas as pd
 
-# Loading Data
+# Loading data
 constructor_results = pd.read_csv('constructor_results.csv')
-constructors = pd.read_csv('constructors.csv')
+all_constructors = pd.read_csv('all_constructors.csv')
 constructor_standings = pd.read_csv('constructor_standings.csv')
 races = pd.read_csv('races.csv')
 
-# Connecting data
-merged_data = pd.merge(constructor_results, constructors, on='constructorId', how='inner')
+# Merging data
+merged_data = pd.merge(constructor_results, all_constructors, on='constructorId', how='inner')
 merged_data = pd.merge(merged_data, constructor_standings, on=['raceId', 'constructorId'], how='inner')
 merged_data = pd.merge(merged_data, races, on='raceId', how='inner')
 
-# Choosing columns
-clean_data = merged_data[['name_y', 'year', 'name_x', 'positionText']]
+# Selecting desired columns
+clean_data = merged_data[['constructorId', 'year', 'name_x', 'positionText', 'relation']]
 
-# Naming columns
-clean_data = clean_data.rename(columns={'name_y': 'race_name', 'name_x': 'constructor_name'})
-# Sorting my new data
+# Sorting data
 clean_data = clean_data.sort_values(by='year')
 
-
-# testing
+# Displaying and saving the result
 print(clean_data.head(20))
-
 clean_data.to_csv('clean_data.csv', index=False)
 
 
