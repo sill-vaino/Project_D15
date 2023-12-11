819affcb93b4286eaf59a50259777c526afc1833
sill-vaino <73522648+sill-vaino@users.noreply.github.com>
2023-12-11 09:08:39 +0200

    Update data_separation.py
    My data is very sensitive regarding time, so I had to shorten up the periods, that way I can make my model more accurate I hope

--- data_separation.py
+++ data_separation.py
@@ -1,15 +1,24 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-# data
-clean_data = pd.read_csv('clean_data.csv')
+# Load the data (replace 'clean_data.csv' with your data file name)
+data = pd.read_csv('clean_data.csv')
 
-# Making three separate data segments
-period_1 = clean_data[clean_data['year'].between(1958, 1976)]
-period_2 = clean_data[clean_data['year'].between(1977, 1995)]
-period_3 = clean_data[clean_data['year'].between(1996, 2023)]
+# Set the number of periods
+periods = 4
 
-# Creating new data files:
-period_1.to_csv('clean_data_period_1.csv', index=False)
-period_2.to_csv('clean_data_period_2.csv', index=False)
-period_3.to_csv('clean_data_period_3.csv', index=False)
+# Split the data into periods
+data_periods = []
+for i in range(periods):
+    start_idx = i * len(data) // periods
+    end_idx = (i + 1) * len(data) // periods
+    period_data = data.iloc[start_idx:end_idx]
+    data_periods.append(period_data)
 
+# Save each period as a separate CSV file
+for i, period_data in enumerate(data_periods):
+    period_data.to_csv(f'clean_data_period_{i + 1}.csv', index=False)
+
+# Show the size of each period
+for i, period_data in enumerate(data_periods):
+    print(f"Number of data points in Period {i + 1}: {len(period_data)}")
